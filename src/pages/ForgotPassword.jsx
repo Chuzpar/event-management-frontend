819a8f12150e3c4789acf74b45a4cc6b556ed85b
Yasin <yasinhassan004@gamil.com>
2026-07-31 05:08:38 +0300

    feat: create forgot password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,39 @@
+import "./ForgotPassword.css";
+import { Link } from "react-router-dom";
+
 function ForgotPassword() {
-  return <h1>Forgot Password</h1>;
+  return (
+    <div className="auth-page">
+      <div className="auth-card">
+
+        <h1>Forgot Password</h1>
+
+        <p>
+          Enter your email address and we'll send you a password reset link.
+        </p>
+
+        <form>
+
+          <input
+            type="email"
+            placeholder="Enter your email"
+          />
+
+          <button type="submit">
+            Send Reset Link
+          </button>
+
+        </form>
+
+        <br />
+
+        <Link to="/login">
+          Back to Login
+        </Link>
+
+      </div>
+    </div>
+  );
 }
 
 export default ForgotPassword;
